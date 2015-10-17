3a505ece5951390f17e953e3b845076bc3530be8
Thomas Kee <t.kee@temperednetworks.com>
2015-10-16 21:48:09 -0700

    adjust for non rails functions

--- lib/api.rb
+++ lib/api.rb
@@ -3,10 +3,12 @@
 # File: lib/api.rb
 #
 #################################################################
-
+# dispatch restful calls to enable provider connector  and return 
+# json responses
 # require 'cloudconnector'  # Service Provider Instance Wrapper
 
 class Api
+# roouters and needed variables to service the rest interface
   def self.routes
     [
       { method: "GET",  		path: %r{^/v1/instance/listall}, 			api_method: 	:listall	},
@@ -21,10 +23,19 @@ class Api
     ]    
   end
 
+# Modeling based on this nice little aws broker
 # https://github.com/cloudfoundry-samples/go_service_broker/blob/master/client/aws.go
-# :
+#
+# implement is small so perhaps we can evaluate and test aws ec2 v2 or fog-aws 
+# https://github.com/fog/fog-aws/blob/master/tests/requests/compute/instance_attrib_tests.rb
+#
+# http://docs.aws.amazon.com/sdkforruby/api/index.html
+#
 # Get all instances
+
+# list all instance
   def self.listall(params)
+# make connectors to providers with focus on aws and rackspace
     puts "CloudConnector.listall(params[:id])"
   end
 
@@ -55,7 +66,8 @@ class Api
     puts "CloudConnector.connect(params[:id])"
   end
 
-# cloudfoundation
+
+# cloudfoundation interface - there are alot of vpc work to set up an instance: json template and parameter file are inputs
 # http://docs.aws.amazon.com/sdkforruby/api/Aws/CloudFormation/Client.html
   def self.deploy(params)
     puts "CloudConnector.stack_create(params[:id])"
